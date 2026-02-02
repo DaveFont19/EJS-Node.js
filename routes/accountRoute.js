3bc0c70982a7bc8845b0aa650f43fb88ff457e92
DaveFont19 <davidfontes20@gmail.com>
2026-02-02 14:01:11 -0600

    Learning Activities: Inserting and Validating Data

---
+++ routes/accountRoute.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = new express.Router();
+const accountController = require("../controllers/accountController");
+const utilities = require("../utilities/");
+const regValidate = require("../utilities/account-validation");
+
+// Route to display the login view
+router.get("/login", utilities.handleErrors(accountController.buildLogin));
+// Route to display the registration view
+router.get(
+  "/register",
+  utilities.handleErrors(accountController.buildRegister),
+);
+// Process the registration data
+router.post(
+  "/register",
+  regValidate.registationRules(),
+  regValidate.checkRegData,
+  utilities.handleErrors(accountController.registerAccount),
+);
+
+module.exports = router;
